18411644f920bb0017771219a57b4ad29cae2494
Cesar Francisco Chavez Maldonado <cesarchamal@gmail.com>
2024-01-11 05:53:13 +0100

    Update.

--- backend/apps/web/internal/db.py
+++ backend/apps/web/internal/db.py
@@ -1,6 +1,10 @@
-import os
-from peewee import SqliteDatabase
+#import os
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#B_PATH = os.path.join(BASE_DIR, 'data', 'ollama.db')
+#DB = SqliteDatabase(DB_PATH)
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DB_PATH = os.path.join(BASE_DIR, 'data', 'ollama.db')
-DB = SqliteDatabase(DB_PATH)
+
+from peewee import *
+
+DB = SqliteDatabase("/content/backend/data/ollama.db")
+DB.connect()
