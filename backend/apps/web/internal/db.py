c46e811a4e8e61fffdfb60dd8b39468f664954b2
Cesar Francisco Chavez Maldonado <cesarchamal@gmail.com>
2024-01-11 05:20:45 +0100

    Update.

--- backend/apps/web/internal/db.py
+++ backend/apps/web/internal/db.py
@@ -1,4 +1,8 @@
-from peewee import *
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DB_PATH = os.path.join(BASE_DIR, 'data', 'ollama.db')
+DB = SqliteDatabase(DB_PATH)
+#from peewee import *
 
-DB = SqliteDatabase("./data/ollama.db")
-DB.connect()
+#DB = SqliteDatabase("./data/ollama.db")
+#DB.connect()
